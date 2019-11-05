5c03e7227372f64d4894f4db30bb5f6fbbb9470c
minikin <djminikin@gmail.com>
2019-11-05 11:31:19 +0100

    Add Store

---
+++ Sources/SwiftComposable/Store.swift
@@ -0,0 +1,57 @@
+//
+//  Store.swift
+//  
+//
+//  Created by Sasha Prokhorenko on 05.11.19.
+//
+
+import Combine
+import SwiftUI
+
+public typealias Reducer<Value, Action> = (inout Value, Action) -> [Effect<Action>]
+
+
+public final class Store<Value, Action>: ObservableObject {
+  private let reducer: Reducer<Value, Action>
+  @Published public private(set) var value: Value
+  private var cancellable: Cancellable?
+
+  public init(initialValue: Value, reducer: @escaping Reducer<Value, Action>) {
+    self.reducer = reducer
+    self.value = initialValue
+  }
+
+  public func send(_ action: Action) {
+    let effects = self.reducer(&self.value, action)
+    effects.forEach { effect in
+      effect.run(self.send)
+    }
+  }
+
+  public func view<LocalValue, LocalAction>(
+    value toLocalValue: @escaping (Value) -> LocalValue,
+    action toGlobalAction: @escaping (LocalAction) -> Action
+  ) -> Store<LocalValue, LocalAction> {
+    let localStore = Store<LocalValue, LocalAction>(
+      initialValue: toLocalValue(self.value),
+      reducer: { localValue, localAction in
+        self.send(toGlobalAction(localAction))
+        localValue = toLocalValue(self.value)
+        return []
+    }
+    )
+    localStore.cancellable = self.$value.sink { [weak localStore] newValue in
+      localStore?.value = toLocalValue(newValue)
+    }
+    return localStore
+  }
+}
+
+public func combine<Value, Action>(
+  _ reducers: Reducer<Value, Action>...
+) -> Reducer<Value, Action> {
+  return { value, action in
+    let effects = reducers.flatMap { $0(&value, action) }
+    return effects
+  }
+}
